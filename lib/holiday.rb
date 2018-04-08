60fcc93fe8aad01298e15651eec5599956c4dd9f
Heidi Hlavinka <hchlav@gmail.com>
2018-04-08 23:24:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,6 +89,7 @@ def all_holidays_with_bbq(holiday_hash)
       holidays.collect do |holiday, supplies|
         if supplies.include?("BBQ")
           holiday.compact
+        end
     end
   end
 end
