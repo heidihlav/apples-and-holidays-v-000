6978c9e301ed6dfcb7952f052aef0aff19c53f8b
Heidi Hlavinka <hchlav@gmail.com>
2018-04-08 22:59:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holidays|
-    if holidays.values == "BBQ"
+    # if holidays.values == "BBQ"
       binding.pry
     # holidays.each do |holiday, supplies|
 
